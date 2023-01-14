555ae09b8ef5f1efae2bcf67d8466c99acc7eb54
unknown <marianaalmada15@outlook.com>
2023-01-14 13:55:56 -0300

    ejercicio 12 terminado

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,24 @@
-const findTheOldest = function() {
+function getAge(birth, death) {
+    if(!death) {
+        death = new Date().getFullYear();
+    }
+    return death - birth;
+}
 
+const findTheOldest = function(people) {
+
+    const oldestPerson = people.reduce((oldest, currentPerson) => {
+        const oldestAge = getAge(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentPersonAge = getAge(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        
+        if(oldestAge < currentPersonAge) {
+            return currentPerson;
+        } else {
+            return oldest;
+        }
+    });
+
+    return oldestPerson;
 };
 
 // Do not edit below this line
